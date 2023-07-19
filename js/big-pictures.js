7fca815ebf71a0a9b69db56c97ce719b5eaa2dfc
StMarina <97902689+StMarina@users.noreply.github.com>
2023-07-19 10:59:27 +0300

    Выполняет ДЗ Открывается и щакрывается (часть 2)

--- js/big-pictures.js
+++ js/big-pictures.js
@@ -1,7 +1,21 @@
 import {setImgProps, isEscButton} from './functions.js';
 
+// Константа задает количество комментариев,
+// выводимых при показе окна / одном нажатии на кнопку (не более чем)
+const COMMENTS_PER_OUTPUT = 5;
+
 // Окно для отображения большой фотографии
 const bigPicture = document.querySelector('.big-picture');
+// Иконка "крестик" закрытия окна
+const closeButton = document.querySelector('.big-picture__cancel');
+// Комментарии вставляются в блок .social__comments.
+const pictureComments = document.querySelector('.social__comments');
+// Блок загрузки новых комментариев .comments-loader
+const commentsLoader = document.querySelector('.comments-loader');
+
+// Переменная под функцию, которая доабвляет комментарии под фотография
+// Ссылка на функцию записывается в переменную при открытии
+let processComments;
 
 /*
  * Небольшая сервисная функция, выполняющая отображение / скрытие
@@ -30,19 +44,6 @@ const setShowModal = (modal = true) => {
 };
 
 /*
- * Функция показывает (добавляет) сгенерированный список комментариев
- * commentsFragment в окне просмотра фотографии
- */
-const showCommentsFragment = (commentsFragment) => {
-  // Комментарии должны вставляться в блок .social__comments.
-  const pictureComments = document.querySelector('.social__comments');
-  // На случай, если там уже был какой то текст комментариев, надо почистить содержимое
-  pictureComments.innerHTML = '';
-  // Добавление нужного списка комментариев
-  pictureComments.append(commentsFragment);
-};
-
-/*
  * Функция создает комментарий по информации (аватар, имя автора, текст)
  */
 const getComment = ({avatar, name, message}) => {
@@ -61,19 +62,54 @@ const getComment = ({avatar, name, message}) => {
 };
 
 /*
- * Функция заполняет список комментариев под фотографией.
- * Комментарии передаются в виде массива объектов через параметр comments.
+ * Функция выводит нужный текст в счетчик комментариев исходя
+ * из параметров (visible - количество показанных комментариев, total - общее)
  */
-const processComments = (comments) => {
+const setCounterText = (visible, total) => {
+  // Блок счётчика комментариев .social__comment-count
+  const commentsCounter = document.querySelector('.social__comment-count');
+  commentsCounter.innerHTML =
+    `${visible} из <span class="comments-count">${total}</span>`;
+};
+
+/*
+ * Функция добавляет переданный массив комментариев под фотографию.
+ * Возвращает количество добавленных комментариев (размер массива)
+ */
+const appendComments = (comments) => {
   // Сначала создается фрагмент для списка комментариев
   const commentFragment = document.createDocumentFragment();
   // В цикле генерятся комментарии и добавляются во фрагмент
   comments.forEach((comment) => {
     commentFragment.append(getComment(comment));
   });
-  // И в конце вызывается отдельная функция, которая добавляет
-  // сгенерированный фрагмент комментариев в нужное место на странице
-  showCommentsFragment(commentFragment);
+  // сгенерированный фрагмент комментариев добавляется в нужное место на странице
+  pictureComments.append(commentFragment);
+  return comments.length;
+};
+
+/*
+ * Инициализация вывода комментариев, тут используется замыкание,
+ * возвращается функция, которая потом и будет выводить порции комментариев
+ */
+const initComments = (comments) => {
+  // Количество уже выведенных комментариев
+  let visibleCommentsCount = 0;
+  // Копия исходного массива комментариев в новый
+  // В нем будут хранится только те комменты, которые пока не выведены
+  const hiddenComments = comments.slice();
+
+  return function() {
+    // Вывод очередной порции комментариев, она вырезается из массива и публикуется,
+    // Счетчик показанных комментариев увеличивается на фактическое количество опубликованных
+    visibleCommentsCount += appendComments(hiddenComments.splice(0, COMMENTS_PER_OUTPUT));
+    // Обновление счетчика комментариев
+    setCounterText(visibleCommentsCount, comments.length);
+    // Если все комментарии выведены, то кнопка публикации скрывается
+    if (visibleCommentsCount === comments.length) {
+      showHideObject(commentsLoader, false);
+    }
+  };
 };
 
 /*
@@ -92,9 +128,23 @@ const processBigPicture = ({url, description, likes, comments}) => {
   // Описание фотографии description вставляется строкой в блок .social__caption
   bigPicture.querySelector('.social__caption').textContent = description;
   // Заполнение комментариев под фотографией
-  processComments(comments);
+  // Очистка имеющегося списка комментариев под фотографией
+  pictureComments.innerHTML = '';
+  // Отображение / скрытие кнопки публикации следующей порции комментариев
+  // В момент инициализации показывать эту кнопку имеет смысл только в том случае,
+  // если количество комментариев в массиве больше одной выводимой порции.
+  // В противном случае кнопка не будет ражиматься ни разу и ее можно не показывать
+  showHideObject(commentsLoader, comments.length > COMMENTS_PER_OUTPUT);
+  // В переменную помещается ссылка на функцию добавления комментариев
+  processComments = initComments(comments);
+  if (comments.length > 0) {
+    // Если в массиве есть хоть один комментарий, то выводим первую порцию
+    processComments();
+  } else {
+    // В противном случае обнуляем счетчик
+    setCounterText(0, 0);
+  }
 };
-
 /*
  * Функция выполняет отрисовку окна с полноразмерным изображением.
  * Предназначена для вызова по клику на миниатюре фотографии.
@@ -107,10 +157,6 @@ const showBigPicture = (pictureData) => {
   // После открытия окна тегу <body> необходимо добавить класс modal-open,
   // чтобы контейнер с фотографиями позади не прокручивался при скролле.
   setShowModal();
-  // После открытия окна необходимо скрыть блоки счётчика комментариев .social__comment-count
-  showHideObject(document.querySelector('.social__comment-count'), false);
-  // а также блок загрузки новых комментариев .comments-loader
-  showHideObject(document.querySelector('.comments-loader'), false);
   // Запуск отдельной функции для заполнения окна изображения данными
   processBigPicture(pictureData);
   // Обработчик закрытия окна по кнопке Esc
@@ -118,7 +164,7 @@ const showBigPicture = (pictureData) => {
 };
 
 /*
- * Функция скрывает окна с полноразмерным изображением
+ * Функция скрывает окно с полноразмерным изображением
  */
 const hideBigPicture = () => {
   // Для скрытия окна у элементу .big-picture добавляется класс hidden
@@ -139,6 +185,13 @@ const onCloseButtonClick = () => {
 };
 
 /*
+ * Обработчик события нажатия на кнопку публикации следующей порции комментариев
+ */
+const onCommentsLoaderClick = () => {
+  processComments();
+};
+
+/*
  * Обработчик нажатия клавиши Esc для закрытия окна просмотра фотогарфии.
  * Здесь нельзя использовать стрелку, можно только декларацию,
  * так как функция используется выше по коду. В противном случае будет ошибка.
@@ -150,20 +203,9 @@ function onEscKeyDown(evt) {
   }
 }
 
-/*
- * Все действия, которые необходимо выполнить один раз для окна просмотра
- * вынесены в отдельную функцию, которая будет запущена в конце модуля.
- * Типа такая инициализация. Пока туда попало только навешивание обработчика на
- * кнопку - крестик. Со временем возможно что то еще добавится.
- */
-const initBigPicture = () => {
-  // Иконка "крестик" закрытия окна
-  const closeButton = document.querySelector('.big-picture__cancel');
-  // Добавление обработчик для закрытия окна
-  closeButton.addEventListener('click', onCloseButtonClick);
-};
-
-// Инициализация модуля
-initBigPicture();
+// Добавление обработчика для закрытия окна
+closeButton.addEventListener('click', onCloseButtonClick);
+// Добавление обработчика для отображения дополнительной порции комментариев
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
 
 export {showBigPicture};
